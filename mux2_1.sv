ff7c8ed56ea0a44bea67dd827435d0c625655a92
Ananya Bairy <ananyabairy8@gmail.com>
2026-02-14 00:32:39 +0530

    Create mux2_1.sv

---
+++ mux2_1.sv
@@ -0,0 +1,18 @@
+
+//------------------------------------------------------------------------------
+//File       : mux2to1.sv
+//Author     : Ananya Bairy
+//Created    : 2026-02-10
+//Module     : and_gate
+//Project    : SystemVerilog and Verification (23EC6PE2SV),
+//Faculty    : Prof. Ajaykumar Devarapalli
+//Description:2-input Multipexer used for basic functional coverage example.
+//------------------------------------------------------------------------------
+
+module mux2to1 (
+  input logic [7:0] a, b,
+  input logic sel,
+  output logic [7:0] y
+);
+  assign y = sel ? b : a;
+endmodule
